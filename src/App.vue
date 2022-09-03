3e7d37093d4996b46150e7720a27faa6fee1c8d1
Li <1643565650@qq.com>
2022-09-03 16:06:37 +0800

    地图优化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="box" :style="{ background: `url(${backgroundImg})` }">
     <div class="box-left"></div>
-    <div class="box-center"></div>
+    <div class="box-center" id="china"></div>
     <div class="box-right"></div>
   </div>
 </template>
@@ -9,21 +9,155 @@
 <script setup lang="ts">
 import backgroundImg from "./assets/背景图片.jpg";
 import { useStore } from "./store";
-// 怎么使用pinia调用
+import { onMounted } from "vue";
+import * as echarts from "echarts";
+import "../src/assets/china";
+
 const store = useStore();
 store.getList();
-</script>
 
-<!-- <script lang="ts">
-import backgroundImg from "./assets/背景图片.jpg";
-export default {
-  setup() {
-    return {
-      backgroundImg,
-    };
-  },
-};
-</script> -->
+onMounted(() => {
+  // 基于准备好的dom，初始化echarts实例
+  const charts = echarts.init(document.querySelector("#china") as HTMLElement);
+  var data = [
+    {
+      name: "内蒙古",
+      itemStyle: {
+        areaColor: "#56b1da",
+      },
+      value: [110.3467, 41.4899],
+    },
+  ];
+  // 绘制图表
+  charts.setOption({
+    backgroundColor: "black",
+    geo: {
+      map: "china",
+      aspectScale: 0.8,
+      layoutCenter: ["50%", "50%"],
+      layoutSize: "123%",
+      itemStyle: {
+        areaColor: {
+          type: "linear-gradient",
+          x: 0,
+          y: 1200,
+          x2: 1000,
+          y2: 0,
+          colorStops: [
+            {
+              offset: 0,
+              color: "#152E6E", // 0% 处的颜色
+            },
+            {
+              offset: 1,
+              color: "#0673AD", // 50% 处的颜色
+            },
+          ],
+          global: true, // 缺省为 false
+        },
+        shadowColor: "#0f5d9d",
+        shadowOffsetX: 0,
+        shadowOffsetY: 15,
+        opacity: 0.5,
+      },
+      emphasis: {
+        areaColor: "#0f5d9d",
+      },
+
+      regions: [
+        {
+          name: "南海诸岛",
+          itemStyle: {
+            areaColor: "rgba(0, 10, 52, 1)",
+            borderColor: "rgba(0, 10, 52, 1)",
+
+            opacity: 0,
+            label: {
+              show: false,
+              color: "#009cc9",
+            },
+          },
+          label: {
+            show: true,
+            // show: false,
+            color: "#FFFFFF",
+            fontSize: 12,
+          },
+        },
+      ],
+    },
+    series: [
+      {
+        type: "map",
+        selectedMode: "multiple",
+        map: "china",
+        aspectScale: 0.75,
+        // aspectScale: 8,
+        layoutCenter: ["50%", "50%"], //地图位置
+        layoutSize: "120%",
+        zoom: 1, //当前视角的缩放比例
+        // roam: true, //是否开启平游或缩放
+        scaleLimit: {
+          //滚轮缩放的极限控制
+          min: 1,
+          max: 2,
+        },
+        label: {
+          // 是否显示省份
+          show: true,
+          color: "#FFFFFF",
+          // 省份问题大小
+          fontSize: 14,
+        },
+        itemStyle: {
+          areaColor: "#0c3653",
+          borderColor: "#1cccff",
+          // 省份边界
+          borderWidth: 1,
+        },
+        emphasis: {
+          areaColor: "#56b1da",
+          label: {
+            // show: true,
+            // show: false,
+            color: "#fff",
+          },
+        },
+        data: data,
+      },
+      {
+        type: "scatter",
+        coordinateSystem: "geo",
+        //   symbol: 'image://http://ssq168.shupf.cn/data/biaoji.png',
+        // symbolSize: [30,120],
+        // symbolOffset:[0, '-40%'] ,
+        label: {
+          // 是否显示坐标
+          show: true,
+        },
+        itemStyle: {
+          // color: "#D8BC37", //标志颜色
+          color: "red", //标志颜色
+        },
+        data: data,
+        showEffectOn: "render",
+        rippleEffect: {
+          // 波纹设置
+          brushType: "stroke",
+        },
+        emphasis: {
+          scale: true,
+        },
+        zlevel: 1,
+        symbol: "pin",
+        symbolSize: [45, 45],
+        // symbolSize: [27, 30],
+        symbolOffset: [0, "-10%"],
+      },
+    ],
+  });
+});
+</script>
 
 <style lang="less">
 * {
@@ -43,7 +177,7 @@ body,
   // border: 3px solid blue;
   display: flex;
   &-left {
-    width: 400px;
+    width: 100px;
     // background: red;
   }
   &-center {
@@ -51,7 +185,7 @@ body,
     flex: 1;
   }
   &-right {
-    width: 400px;
+    width: 100px;
     // background: blue;
   }
 }
