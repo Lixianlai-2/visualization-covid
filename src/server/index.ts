15ac2e43b4fefcfafb1d9cf6debd325c5aebc8d9
Li <1643565650@qq.com>
2022-09-06 10:14:29 +0800

    不再通过本地后台请求，直接请求腾讯疫情数据

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,15 @@
 import axios from "axios";
 
-const server = axios.create({
-  // 添加到本地服务器接口，前提是打开本地服务器
-  baseURL: "http://localhost:3333",
-});
+// const server = axios.create({
+//   // 添加到本地服务器接口，前提是打开本地服务器
+//   // baseURL: "http://localhost:3333",
+// });
 
 // 发送一个get请求并导出
-export const getApiList = () => server.get("/api/list").then((res) => res.data);
+// export const getApiList = () => server.get("/api/list").then((res) => res.data);
+export const getApiList = () =>
+  axios
+    .get(
+      "https://api.inews.qq.com/newsqa/v1/query/inner/publish/modules/list?modules=localCityNCOVDataList,diseaseh5Shelf"
+    )
+    .then((res) => res.data.data);
